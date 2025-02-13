04b9ab21d041a97f730c1d87261c07e070d5b554
Ellen Lee <ellenlee@8ball.local>
2025-02-13 12:59:20 -1000

    enable ray trace, global extrema seems to work now

--- src/us_slicer_std.c
+++ src/us_slicer_std.c
@@ -240,50 +240,21 @@ int __declspec(dllexport) APIENTRY UserDefinedSurface5(USER_DATA *UD, FIXED_DATA
 
       case 5:
       	/* ZEMAX wants a real ray trace to this surface */
-         // ray_in.xt = (UD->x); ray_in.yt = (UD->y);
-         // ray_in.l = (UD->l); ray_in.m = (UD->m); ray_in.n = (UD->n);
+         ray_in.xt = (UD->x); ray_in.yt = (UD->y);
+         ray_in.l = (UD->l); ray_in.m = (UD->m); ray_in.n = (UD->n);
 
-         // // The first thing this function does is reset the members of ray_out
-         // // to be all NANs
-         // RayTraceSlicer(&ray_out, ray_in, zmin_GLOBAL, zmax_GLOBAL,
-         //    p, sag_func, transfer_dist_func, surf_normal_func);
+         // The first thing this function does is reset the members of ray_out
+         // to be all NANs
+         RayTraceSlicer(&ray_out, ray_in, zmin_GLOBAL, zmax_GLOBAL,
+            p, sag_func, transfer_dist_func, surf_normal_func);
 
-         // // Ray missed if transfer distance or normal vector could not be computed
-         // if (isnan(ray_out.t) || isnan(ray_out.ln)) return (FD->surf);
+         // Ray missed if transfer distance or normal vector could not be computed
+         if (isnan(ray_out.t) || isnan(ray_out.ln)) return (FD->surf);
 
-         // (UD->x) = ray_out.xs; (UD->y) = ray_out.ys; (UD->z) = ray_out.zs;
-         // (UD->path) = ray_out.t;
-         // (UD->ln) = ray_out.ln; (UD->mn) = ray_out.mn; (UD->nn) = ray_out.nn;
+         (UD->x) = ray_out.xs; (UD->y) = ray_out.ys; (UD->z) = ray_out.zs;
+         (UD->path) = ray_out.t;
+         (UD->ln) = ray_out.ln; (UD->mn) = ray_out.mn; (UD->nn) = ray_out.nn;
 
-         // if (Refract(FD->n1, FD->n2, &UD->l, &UD->m, &UD->n, UD->ln, UD->mn, UD->nn)) return(-FD->surf);
-
-         /* ZEMAX wants a real ray trace to this surface */
-         if (FD->cv == 0.0)
-         	{
-	         UD->ln =  0.0;
-   	      UD->mn =  0.0;
-      	   UD->nn = -1.0;
-			   if (Refract(FD->n1, FD->n2, &UD->l, &UD->m, &UD->n, UD->ln, UD->mn, UD->nn)) return(-FD->surf);
-            return(0);
-            }
-         /* okay, not a plane. */
-			a = (UD->n) * (UD->n) * FD->k + 1;
-			b = ((UD->n)/FD->cv) - (UD->x) * (UD->l) - (UD->y) * (UD->m);
-			c = (UD->x) * (UD->x) + (UD->y) * (UD->y);
-			rad = b * b - a * c;
-			if (rad < 0) return(FD->surf);  /* ray missed this surface */
-			if (FD->cv > 0) t = c / (b + sqrt(rad));
-			else           t = c / (b - sqrt(rad));
-			(UD->x) = (UD->l) * t + (UD->x);
-			(UD->y) = (UD->m) * t + (UD->y);
-			(UD->z) = (UD->n) * t + (UD->z);
-			UD->path = t;
-			zc = (UD->z) * FD->cv;
-			rad = zc * FD->k * (zc * (FD->k + 1) - 2) + 1;
-			casp = FD->cv / sqrt(rad);
-			UD->ln = (UD->x) * casp;
-			UD->mn = (UD->y) * casp;
-			UD->nn = ((UD->z) - ((1/FD->cv) - (UD->z) * FD->k)) * casp;
          if (Refract(FD->n1, FD->n2, &UD->l, &UD->m, &UD->n, UD->ln, UD->mn, UD->nn)) return(-FD->surf);
 
          break;
